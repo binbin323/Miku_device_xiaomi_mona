68f886e5569e6e67604d9efa3beb1bb10d94cddc
binbin323 <bin@dhao.cc>
2026-04-22 12:13:37 +0800

    mona: Import vendor/xiaomi/mojin namespace

--- extract-files.py
+++ extract-files.py
@@ -17,6 +17,7 @@ namespace_imports = [
     'device/xiaomi/mojin',
     'hardware/qcom-caf/sm8350',
     'hardware/qcom-caf/wlan',
+    'vendor/xiaomi/mojin',
     'vendor/qcom/opensource/commonsys/display',
     'vendor/qcom/opensource/commonsys-intf/display',
     'vendor/qcom/opensource/display',
